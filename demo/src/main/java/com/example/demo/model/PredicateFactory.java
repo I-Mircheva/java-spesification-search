0198d9ea519b7b1925448ac1e4abcf58221e14be
sofia-71 <sofia-71@MB27898.local>
2018-08-22 15:37:09 +0300

    Optimizing PredicateFactory

--- demo/src/main/java/com/example/demo/model/PredicateFactory.java
+++ demo/src/main/java/com/example/demo/model/PredicateFactory.java
@@ -23,43 +23,17 @@ public class PredicateFactory {
 
     public static Specification<Customer> build(MultiValueMap<String, String> parameters){
 
-        List<SpecSearchCriteria> readySpecs = new ArrayList<>();
-
-
-        for (Map.Entry<String, List<String>> entry : parameters.entrySet()) {
-
-            // [ pets.name-EQ ] becomes (hopefully) [ pets.name ] [ EQ ]
-            String[] pathKeyAndOperation = entry.getKey().split("-");
-
-            // [ pets.name ] becomes (hopefully) [ pets ] [ name ]
-            List<String> list = new ArrayList<>(Arrays.asList(pathKeyAndOperation[0].split("\\.")));
-
-            int indexOfKey = list.size() - 1;
-            // key is [ name ]
-            String key = list.remove(indexOfKey);
-
-            // Join the path to use later with "." delimiter
-            String path = String.join(".", list);
-
-            SpecSearchCriteria singleSpec = new SpecSearchCriteria(path, key, operations.get(pathKeyAndOperation[1]), entry.getValue());
-            readySpecs.add(singleSpec);
-        }
-
-        Map<String, List<SpecSearchCriteria>> searchCriteriaListGroupedByPath = readySpecs.stream().collect(
-                Collectors.groupingBy(SpecSearchCriteria::getJoinPath)
-        );
-
-//        readySpecs.
 
         Specification<Customer> result = null;
 
-        for(List<SpecSearchCriteria> one : searchCriteriaListGroupedByPath.values()) {
+        for(List<SpecSearchCriteria> one : grouping(parameters).values()) {
             result = Specification.where(result)
                     .and(groupSpec(one));
         }
 
         return result;
     }
+
 
     public static Specification<Customer> groupSpec(List<SpecSearchCriteria> groupedSpecs) {
 
@@ -134,6 +108,32 @@ public class PredicateFactory {
 //
 //        return null;
 //    }
+    public static Map<String, List<SpecSearchCriteria>> grouping(MultiValueMap<String, String> parameters){
+        List<SpecSearchCriteria> readySpecs = new ArrayList<>();
+        for (Map.Entry<String, List<String>> entry : parameters.entrySet()) {
+
+            // [ pets.name-EQ ] becomes (hopefully) [ pets.name ] [ EQ ]
+            String[] pathKeyAndOperation = entry.getKey().split("-");
+
+            // [ pets.name ] becomes (hopefully) [ pets ] [ name ]
+            List<String> list = new ArrayList<>(Arrays.asList(pathKeyAndOperation[0].split("\\.")));
+
+            int indexOfKey = list.size() - 1;
+            // key is [ name ]
+            String key = list.remove(indexOfKey);
+
+            // Join the path to use later with "." delimiter
+            String path = String.join(".", list);
+
+            SpecSearchCriteria singleSpec = new SpecSearchCriteria(path, key, operations.get(pathKeyAndOperation[1]), entry.getValue());
+            readySpecs.add(singleSpec);
+        }
+
+        Map<String, List<SpecSearchCriteria>> searchCriteriaListGroupedByPath = readySpecs.stream().collect(
+                Collectors.groupingBy(SpecSearchCriteria::getJoinPath)
+        );
+        return searchCriteriaListGroupedByPath;
+    }
 }
 
 
