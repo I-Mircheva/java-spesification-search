be712c688a4e843e5d33e045c81fa0ead3e6e87f
sofia-65 <sofia-65@MB27892.local>
2018-08-23 16:23:38 +0300

    Add tests

--- demo/src/main/java/com/example/demo/model/PrepareDB.java
+++ demo/src/main/java/com/example/demo/model/PrepareDB.java
@@ -17,51 +17,51 @@ public class PrepareDB {
 
     @PostConstruct
     public void prepareDB() {
-        repository.deleteAll();
-        {
-            Customer customer1 = new Customer("Jack", "Bauer", 12L);
-            ArrayList<Pet> pets = new ArrayList<>();
-            pets.add(new Pet("Boki", 130, "Dragon"));
-            pets.add(new Pet("Jack", 87, "Dog"));
-            customer1.setPets(pets);
-            repository.save(customer1);
-        }
-
-        {
-            Customer customer2 = new Customer("Chloe", "Brian", 13L);
-            ArrayList<Pet> pets = new ArrayList<>();
-            pets.add(new Pet("Joki", 10, "Snake"));
-            pets.add(new Pet("Sack", 77, "Cat"));
-            customer2.setPets(pets);
-            repository.save(customer2);
-        }
-
-        {
-            Customer customer3 = new Customer("Kim", "Bauer", 10L);
-            ArrayList<Pet> pets = new ArrayList<>();
-            pets.add(new Pet("Joki", 1, "Bird"));
-            pets.add(new Pet("Joki", 3, "Leopard"));
-            pets.add(new Pet("Sack", 5, "Cat"));
-            customer3.setPets(pets);
-            repository.save(customer3);
-        }
-
-        {
-            Customer customer4 = new Customer("David", "Palmer", 5L);
-            ArrayList<Pet> pets = new ArrayList<>();
-            pets.add(new Pet("Beauty", 1, "Caterpillar"));
-            pets.add(new Pet("Ugly", 2, "Butterfly"));
-            customer4.setPets(pets);
-            repository.save(customer4);
-        }
-
-        {
-            Customer customer5 = new Customer("Michelle", "Dessler", 30L);
-            ArrayList<Pet> pets = new ArrayList<>();
-            pets.add(new Pet("Jaki", 10, "Snake"));
-            pets.add(new Pet("Sack", 77, "Cat"));
-            customer5.setPets(pets);
-            repository.save(customer5);
-        }
+//        repository.deleteAll();
+//        {
+//            Customer customer1 = new Customer("Jack", "Bauer", 12L);
+//            ArrayList<Pet> pets = new ArrayList<>();
+//            pets.add(new Pet("Boki", 130, "Dragon"));
+//            pets.add(new Pet("Jack", 87, "Dog"));
+//            customer1.setPets(pets);
+//            repository.save(customer1);
+//        }
+//
+//        {
+//            Customer customer2 = new Customer("Chloe", "Brian", 13L);
+//            ArrayList<Pet> pets = new ArrayList<>();
+//            pets.add(new Pet("Joki", 10, "Snake"));
+//            pets.add(new Pet("Sack", 77, "Cat"));
+//            customer2.setPets(pets);
+//            repository.save(customer2);
+//        }
+//
+//        {
+//            Customer customer3 = new Customer("Kim", "Bauer", 10L);
+//            ArrayList<Pet> pets = new ArrayList<>();
+//            pets.add(new Pet("Joki", 1, "Bird"));
+//            pets.add(new Pet("Joki", 3, "Leopard"));
+//            pets.add(new Pet("Sack", 5, "Cat"));
+//            customer3.setPets(pets);
+//            repository.save(customer3);
+//        }
+//
+//        {
+//            Customer customer4 = new Customer("David", "Palmer", 5L);
+//            ArrayList<Pet> pets = new ArrayList<>();
+//            pets.add(new Pet("Beauty", 1, "Caterpillar"));
+//            pets.add(new Pet("Ugly", 2, "Butterfly"));
+//            customer4.setPets(pets);
+//            repository.save(customer4);
+//        }
+//
+//        {
+//            Customer customer5 = new Customer("Michelle", "Dessler", 30L);
+//            ArrayList<Pet> pets = new ArrayList<>();
+//            pets.add(new Pet("Jaki", 10, "Snake"));
+//            pets.add(new Pet("Sack", 77, "Cat"));
+//            customer5.setPets(pets);
+//            repository.save(customer5);
+//        }
     }
 }
--- demo/src/test/java/com/example/demo/MockMvcExampleTests.java
+++ demo/src/test/java/com/example/demo/MockMvcExampleTests.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import com.example.demo.model.Customer;
+import com.example.demo.model.Pet;
+import com.example.demo.repository.CustomerRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -8,6 +11,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -21,18 +26,89 @@ public class MockMvcExampleTests {
     @Autowired
     private MockMvc mvc;
 
+    @Autowired
+    CustomerRepository repository;
+
+
     @Test
     public void exampleSearchPetsByWeight() throws Exception {
-        this.mvc.perform(get("/customers?pets.weight=3")).andExpect(status().isOk())
+        {
+            Customer customer1 = new Customer("Jack", "Bauer", 12L);
+            ArrayList<Pet> pets = new ArrayList<>();
+            pets.add(new Pet("Boki", 130, "Dragon"));
+            pets.add(new Pet("Jack", 87, "Dog"));
+            customer1.setPets(pets);
+            repository.save(customer1);
+        }
+
+        {
+            Customer customer2 = new Customer("Chloe", "Brian", 13L);
+            ArrayList<Pet> pets = new ArrayList<>();
+            pets.add(new Pet("Joki", 10, "Snake"));
+            pets.add(new Pet("Sack", 77, "Cat"));
+            customer2.setPets(pets);
+            repository.save(customer2);
+        }
+        this.mvc.perform(get("/customers?pets.weight-GOE=78")).andExpect(status().isOk())
                 .andExpect(content().string(
-                        "[{\"id\":1,\"age\":12,\"firstName\":\"Jack\",\"lastName\":\"Bauer\",\"pets\":[{\"id\":2,\"name\":\"Boki\",\"weight\":130,\"type\":\"Dragon\"},{\"id\":3,\"name\":\"Jack\",\"weight\":87,\"type\":\"Dog\"},{\"id\":4,\"name\":\"Jacob\",\"weight\":13,\"type\":\"Cat\"},{\"id\":5,\"name\":\"Koko\",\"weight\":10,\"type\":\"Mouse\"},{\"id\":6,\"name\":\"Sony\",\"weight\":9,\"type\":\"Snake\"}]}]"));
+                        "[{\"id\":1,\"age\":12,\"firstName\":\"Jack\",\"lastName\":\"Bauer\",\"pets\":[{\"id\":2,\"name\":\"Boki\",\"weight\":130,\"type\":\"Dragon\"},{\"id\":3,\"name\":\"Jack\",\"weight\":87,\"type\":\"Dog\"}]}]"));
     }
+
     @Test
-    public void exampleSearchPetsByWeightAndCustomerByName() throws Exception {
-        this.mvc.perform(get("/customers?firstName=Jack&pets.weight=9")).andExpect(status().isOk())
+    public void exampleSearchPetsByPetParamsAndPaging() throws Exception {
+        {
+            Customer customer1 = new Customer("Jack", "Bauer", 12L);
+            ArrayList<Pet> pets = new ArrayList<>();
+            pets.add(new Pet("Boki", 130, "Dragon"));
+            pets.add(new Pet("Joe", 87, "Dog"));
+            customer1.setPets(pets);
+            repository.save(customer1);
+        }
+
+        {
+            Customer customer2 = new Customer("Chloe", "Brian", 13L);
+            ArrayList<Pet> pets = new ArrayList<>();
+            pets.add(new Pet("Joki", 10, "Developer"));
+            pets.add(new Pet("Sack", 77, "Cat"));
+            customer2.setPets(pets);
+            repository.save(customer2);
+        }
+        this.mvc.perform(get("/customers?pets.weight-LT=87&page=0&size=1")).andExpect(status().isOk())
                 .andExpect(content().string(
-                        "[{\"id\":1,\"age\":12,\"firstName\":\"Jack\",\"lastName\":\"Bauer\",\"pets\":[{\"id\":2,\"name\":\"Boki\",\"weight\":130,\"type\":\"Dragon\"},{\"id\":3,\"name\":\"Jack\",\"weight\":87,\"type\":\"Dog\"},{\"id\":4,\"name\":\"Jacob\",\"weight\":13,\"type\":\"Cat\"},{\"id\":5,\"name\":\"Koko\",\"weight\":10,\"type\":\"Mouse\"},{\"id\":6,\"name\":\"Sony\",\"weight\":9,\"type\":\"Snake\"}]}]"));
+                        "[{\"id\":4,\"age\":13,\"firstName\":\"Chloe\",\"lastName\":\"Brian\",\"pets\":[{\"id\":5,\"name\":\"Joki\",\"weight\":10,\"type\":\"Developer\"},{\"id\":6,\"name\":\"Sack\",\"weight\":77,\"type\":\"Cat\"}]}]"));
+        this.mvc.perform(get("/customers?pets.weight-LT=87&page=1&size=1")).andExpect(status().isOk())
+                .andExpect(content().string(
+                        "[]"));
     }
+
+    @Test
+    public void exampleSearchPetsByPetParamsCustomerParamsAndPaging() throws Exception {
+        {
+            Customer customer1 = new Customer("Jacky", "Bauer", 12L);
+            ArrayList<Pet> pets = new ArrayList<>();
+            pets.add(new Pet("Boki", 130, "Dragon"));
+            pets.add(new Pet("Joe", 87, "Dog"));
+            customer1.setPets(pets);
+            repository.save(customer1);
+        }
+
+        {
+            Customer customer2 = new Customer("Chloe", "Brian", 13L);
+            ArrayList<Pet> pets = new ArrayList<>();
+            pets.add(new Pet("Joki", 10, "Developer"));
+            pets.add(new Pet("Sacky", 77, "Cat"));
+            customer2.setPets(pets);
+            repository.save(customer2);
+        }
+        this.mvc.perform(get("/customers?pets.weight-LT=87&page=0&size=1&age=13")).andExpect(status().isOk())
+                .andExpect(content().string(
+                        "[{\"id\":4,\"age\":13,\"firstName\":\"Chloe\",\"lastName\":\"Brian\",\"pets\":[{\"id\":5,\"name\":\"Joki\",\"weight\":10,\"type\":\"Developer\"},{\"id\":6,\"name\":\"Sack\",\"weight\":77,\"type\":\"Cat\"}]}]"));
+        this.mvc.perform(get("/customers?pets.weight-LT=87&page=1&size=1")).andExpect(status().isOk())
+                .andExpect(content().string(
+                        "[]"));
+    }
+
+
 
 
 }
